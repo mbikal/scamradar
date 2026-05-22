588ae0090de199e50b7c99fe60f89e72bb7d654f
mab191207-stack <mab191207@gmail.com>
2026-05-22 11:49:15 +0545

    creating git ignore

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express ();
-const PORT = 3000;
+const cors = require("cors");
+const helmet = require("helmet");
+const PORT = process.env.Server_PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 app.get("/health",(req, res)=>{
     res.status(200).json({message: "Server is healthy"});
@@ -10,7 +15,7 @@ app.get("/health",(req, res)=>{
 app.post("/api/analyze", (req, res)=>{
     const receivedData = req.body;
     console.log("Received data:", receivedData);
-    res.status(200).json({message: "score: 50", data: receivedData});
+    res.status(200).json({riskScore: 50,message: "Analysis complete", data: receivedData});
 })
 
 app.listen(PORT, ()=>{
